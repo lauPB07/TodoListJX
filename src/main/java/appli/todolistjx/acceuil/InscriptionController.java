da16b129269aee5fd0b72a09dddfe699ee2aad0a
lauPB07 <laurapiresberclaz@gmail.com>
2024-09-10 15:10:57 +0200

    feat : création de la view InscriptionView et son controller InscriptionController avec les methodes créer

---
+++ src/main/java/appli/todolistjx/acceuil/InscriptionController.java
@@ -0,0 +1,33 @@
+package appli.todolistjx.acceuil;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class InscriptionController {
+
+    @FXML
+    private TextField comfirmationField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField mdpField;
+
+    @FXML
+    private TextField nomField;
+
+    @FXML
+    private TextField prenomField;
+
+    @FXML
+    void inscription(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retour(ActionEvent event) {
+
+    }
+}
