6b42dcd416916ebaad1a0d85461f345445920749
lauPB07 <laurapiresberclaz@gmail.com>
2024-09-10 14:22:19 +0200

    fix : modification des fichiers Hello-View et HelloCOntroller renommer en LoginView et LoginController

--- src/main/java/appli/todolistjx/StartApplication.java
+++ src/main/java/appli/todolistjx/StartApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("loginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
