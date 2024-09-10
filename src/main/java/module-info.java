e1ad6019039fb010842cb5a0ad8db43addc27dde
lauPB07 <laurapiresberclaz@gmail.com>
2024-09-10 14:11:21 +0200

    Initialisation du projet + fix: modification du nom HelloApplication enStarApplication

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module appli.todolistjx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens appli.todolistjx to javafx.fxml;
+    exports appli.todolistjx;
+}
