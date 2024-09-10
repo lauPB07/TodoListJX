d2fd78b75a99ca7b8cca6d73e00c9b5246a7b67a
lauPB07 <laurapiresberclaz@gmail.com>
2024-09-10 14:43:37 +0200

    fix : modification de la page LoginView, modification du controller LoginController avec l'ajout des methode pour chaques bouttons correspondant et enfin modification de la page StartApplication pour enlever la taille de la premiere page de l'appli

--- src/main/java/appli/todolistjx/StartApplication.java
+++ src/main/java/appli/todolistjx/StartApplication.java
@@ -11,8 +11,8 @@ public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("loginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("TodoList !");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/appli/todolistjx/acceuil/LoginController.java
+++ src/main/java/appli/todolistjx/acceuil/LoginController.java
@@ -1,5 +1,6 @@
 package appli.todolistjx.acceuil;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -7,9 +8,28 @@ import javafx.scene.control.TextField;
 
 public class LoginController {
     @FXML
-    private TextField email;
+    private TextField emailField;
 
     @FXML
-    private PasswordField mdp;
+    private PasswordField mdpField;
+
+    @FXML
+    private Label messageErreurField;
+
+    @FXML
+    void connexion(ActionEvent event) {
+        System.out.println("L email est : "+emailField.getText());
+        System.out.println("Le mot de passe est : "+mdpField.getText());
+    }
+
+    @FXML
+    void inscription(ActionEvent event) {
+
+    }
+
+    @FXML
+    void motDePasseOublie(ActionEvent event) {
+
+    }
 
 }
