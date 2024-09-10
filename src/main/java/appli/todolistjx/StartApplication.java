d9724e82d891ba52a568e24f8dfa84cf6a6576f4
lauPB07 <laurapiresberclaz@gmail.com>
2024-09-10 15:13:43 +0200

    fix : ajout d'une methode changerScene dans StartApplication

--- src/main/java/appli/todolistjx/StartApplication.java
+++ src/main/java/appli/todolistjx/StartApplication.java
@@ -7,7 +7,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class StartApplication extends Application {
+    private static Stage maStage;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("loginView.fxml"));
@@ -19,5 +21,20 @@ public class StartApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void changeScene(String fxml , String nomScene) {
+        maStage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource(fxml+".fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load());
+            maStage.setTitle(nomScene);
+            maStage.setScene(scene);
+            maStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
