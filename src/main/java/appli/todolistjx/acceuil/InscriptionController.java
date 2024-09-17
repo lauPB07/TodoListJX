5a133ad4dfdf7a274353592a4eb3737c62814741
lauPB07 <laurapiresberclaz@gmail.com>
2024-09-17 14:51:12 +0200

    feat : ajout de la methode connexion

--- src/main/java/appli/todolistjx/acceuil/InscriptionController.java
+++ src/main/java/appli/todolistjx/acceuil/InscriptionController.java
@@ -1,8 +1,15 @@
 package appli.todolistjx.acceuil;
 
+import appli.todolistjx.StartApplication;
+import appli.todolistjx.bdd.Bdd;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class InscriptionController {
 
@@ -22,12 +29,40 @@ public class InscriptionController {
     private TextField prenomField;
 
     @FXML
+    private Label messageErreurField;
+
+    @FXML
     void inscription(ActionEvent event) {
+        Bdd connexionBdd = new Bdd();
+        Connection connection = connexionBdd.getBdd();
+
+        if(!mdpField.getText().equals(comfirmationField.getText())){
+           messageErreurField.setText("Erreur");
+
+        }else {
+            String sql1 = "INSERT INTO utilisateur (nom,prenom,email,mdp) VALUES (?,?,?,?) ";
+
+            try {
+                PreparedStatement requete = connection.prepareStatement(sql1);
+                requete.setString(1,nomField.getText());
+                requete.setString(2,prenomField.getText());
+                requete.setString(3,emailField.getText());
+                requete.setString(4,mdpField.getText());
+                requete.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
 
     }
 
     @FXML
     void retour(ActionEvent event) {
-
+        StartApplication.changeScene("acceuil/loginView","Connexion");
     }
+
+
+
 }
