02ea247cb0d0fad0170f4bc9e2df11a5c1a9da02
lauPB07 <laurapiresberclaz@gmail.com>
2024-10-01 14:15:47 +0200

    fix : modification de la page admin + initialisation du tableau

--- src/main/java/appli/todolistjx/acceuil/AcceuilController.java
+++ src/main/java/appli/todolistjx/acceuil/AcceuilController.java
@@ -1,13 +1,33 @@
 package appli.todolistjx.acceuil;
 
+import appli.todolistjx.entity.Liste;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AcceuilController implements Initializable {
+
+    @FXML
+    private TableView<Liste> tableauListe;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        String [][] colonnes = {
+                {"ID Liste","idListe"},
+                {"Nom", "non"}
+        };
+
+        for (int i = 0; i <colonnes.length; i++){
+            TableColumn<Liste,String> maColonne = new TableColumn<>(colonnes[i][0]);
+            maColonne.setCellValueFactory(new PropertyValueFactory<>(colonnes[i][0]));
+            tableauListe.getColumns().add(maColonne);
+        }
 
     }
+
 }
