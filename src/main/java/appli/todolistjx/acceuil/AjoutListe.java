3f7ac83bb4ec602cd035948f2078110c2a3c217f
lauPB07 <laurapiresberclaz@gmail.com>
2024-10-01 14:24:11 +0200

    feat : création de la page ajout de liste + de sa methose pour ajouter une liste

---
+++ src/main/java/appli/todolistjx/acceuil/AjoutListe.java
@@ -0,0 +1,14 @@
+package appli.todolistjx.acceuil;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AjoutListe {
+    @FXML
+    private Label erreurLabel;
+
+    @FXML
+    private TextField nomField;
+
+}
--- src/main/java/appli/todolistjx/repository/ListeRepository.java
+++ src/main/java/appli/todolistjx/repository/ListeRepository.java
@@ -1,4 +1,26 @@
 package appli.todolistjx.repository;
 
+import appli.todolistjx.bdd.Bdd;
+import javafx.scene.control.Label;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ListeRepository {
+    static Bdd connexionBdd = new Bdd();
+    static Connection connection = connexionBdd.getBdd();
+
+    public void ajoutListe(String nom, Label label){
+        String sql1 = "INSERT INTO liste (nom) VALUES (?) ";
+
+        try {
+            PreparedStatement requete = connection.prepareStatement(sql1);
+            requete.setString(1,nom);
+            requete.executeUpdate();
+            label.setText("Nouvelle Liste créer !");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
